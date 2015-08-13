d0952cd29d2d7990c636b6b157cf36a1a35b31fd
Cipriano André dos Santos Freitas <casf1992@gmail.com>
2015-08-13 14:24:03 +0100

    preliminary hardware media keys support

--- ProjectCast/Pages/EpisodesList.xaml.cs
+++ ProjectCast/Pages/EpisodesList.xaml.cs
@@ -2,9 +2,12 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Windows.Media;
+using Windows.Storage.Streams;
 using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -16,6 +19,8 @@ namespace ProjectCast
     /// </summary>
     public sealed partial class EpisodesList : Page
     {
+        private SystemMediaTransportControls systemPlayerControls;
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             Frame rootFrame = Window.Current.Content as Frame;
@@ -36,6 +41,13 @@ namespace ProjectCast
         public EpisodesList()
         {
             this.InitializeComponent();
+
+            systemPlayerControls = SystemMediaTransportControls.GetForCurrentView();
+
+            systemPlayerControls.ButtonPressed += SystemControls_ButtonPressed;
+
+            systemPlayerControls.IsPlayEnabled = true;
+            systemPlayerControls.IsPauseEnabled = true;
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
@@ -67,6 +79,82 @@ namespace ProjectCast
             var selectedEpisode = (Episode)e.ClickedItem;
             PodcastPlayer.Source = new Uri(selectedEpisode.EpisodeUrl);
             PodcastPlayer.Play();
+        }
+
+
+        void SystemControls_ButtonPressed(SystemMediaTransportControls sender,
+    SystemMediaTransportControlsButtonPressedEventArgs args)
+        {
+            switch (args.Button)
+            {
+                case SystemMediaTransportControlsButton.Play:
+                    PlayMedia();
+                    break;
+                case SystemMediaTransportControlsButton.Pause:
+                    PauseMedia();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        async void PlayMedia()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                PodcastPlayer.Play();
+            });
+        }
+
+        async void PauseMedia()
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                PodcastPlayer.Pause();
+            });
+        }
+
+        void MusicPlayer_CurrentStateChanged(object sender, RoutedEventArgs e)
+        {
+            switch (PodcastPlayer.CurrentState)
+            {
+                case MediaElementState.Playing:
+                    systemPlayerControls.PlaybackStatus = MediaPlaybackStatus.Playing;
+                    break;
+                case MediaElementState.Paused:
+                    systemPlayerControls.PlaybackStatus = MediaPlaybackStatus.Paused;
+                    break;
+                case MediaElementState.Stopped:
+                    systemPlayerControls.PlaybackStatus = MediaPlaybackStatus.Stopped;
+                    break;
+                case MediaElementState.Closed:
+                    systemPlayerControls.PlaybackStatus = MediaPlaybackStatus.Closed;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void PodcastPlayer_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            UpdateSongInfoManually();
+        }
+
+        void UpdateSongInfoManually()
+        {
+            // Get the updater.
+            SystemMediaTransportControlsDisplayUpdater updater = systemPlayerControls.DisplayUpdater;
+
+            // Music metadata.
+            //updater.MusicProperties.Title = "song title";
+
+            //// Set the album art thumbnail.
+            //// RandomAccessStreamReference is defined in Windows.Storage.Streams
+            //updater.Thumbnail =
+            //   RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Music/music1_AlbumArt.jpg"));
+
+            // Update the system media transport controls.
+            updater.Update();
         }
     }
 }
